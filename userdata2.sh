8e36d4f5032218375cd04beab90adb30fd8c9567
israrmarwat347 <107938255+israrmarwat347@users.noreply.github.com>
2025-07-04 03:10:25 +0200

    Update userdata2.sh

--- userdata2.sh
+++ userdata2.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 apt update
-apt install -y apache2 awscli
+apt install -y apache2
 
-# Get IMDSv2 token
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
-  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+# Get the instance ID using the instance metadata
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
-# Use the token to fetch instance ID and AMI ID
-INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s \
-  http://169.254.169.254/latest/meta-data/instance-id)
+# Install the AWS CLI
+apt install -y awscli
 
-AMI_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s \
-  http://169.254.169.254/latest/meta-data/ami-id)
+# Download the images from S3 bucket
+aws s3 cp s3://myterraformprojectbucket2023/project.webp /var/www/html/project.png --acl public-read
 
-# Generate HTML file
+# Create a simple HTML file with the portfolio content and display the images
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html>
 <html>
 <head>
   <title>My Portfolio</title>
   <style>
+    /* Add animation and styling for the text */
     @keyframes colorChange {
       0% { color: red; }
       50% { color: green; }
@@ -33,13 +32,12 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <h2>AMI ID: <span style="color:blue">$AMI_ID</span></h2>
-  <p>Welcome to Terraform Project</p>
+  <p>Hey Devops Learner</p>
+
 </body>
 </html>
 EOF
 
-# Start Apache
+# Start Apache and enable it on boot
 systemctl start apache2
 systemctl enable apache2
-
